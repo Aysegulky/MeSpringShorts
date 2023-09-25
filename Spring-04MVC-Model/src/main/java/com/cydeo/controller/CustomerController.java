de4e176c275a71ff4462263b5b38cc2f8660d109
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-09-25 11:54:38 +0300

    Short Video 22-Continue Thymeleaf

--- Spring-04MVC-Model/src/main/java/com/cydeo/controller/CustomerController.java
+++ Spring-04MVC-Model/src/main/java/com/cydeo/controller/CustomerController.java
@@ -1,13 +1,43 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.Model.Customer;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+    @RequestMapping("/register")
+    public String register(Model model){
+
+        model.addAttribute("customer",new Customer());
+        List<String> stateList = Arrays.asList("Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming");
+
+        model.addAttribute("stateList",stateList);
+
+
+        return "customer/register";
+
+    }
+
+    @PostMapping("/confirm")
+    public String confirm(@ModelAttribute("customer")Customer customer, Model model){
+
+        System.out.println(customer);
+        model.addAttribute("customer", customer);
+
+        return "customer/confirm";
+
+
+    }
 
 
 }
