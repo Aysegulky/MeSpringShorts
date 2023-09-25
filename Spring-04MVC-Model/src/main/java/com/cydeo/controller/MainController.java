91304e3ed0714c395a3a5bee8ef1527134d4423c
aysegul <aysegulkafaliyilmaz@gmail.com>
2023-09-25 08:56:33 +0300

    Short Video 18-Continue Thymeleaf

--- Spring-04MVC-Model/src/main/java/com/cydeo/controller/MainController.java
+++ Spring-04MVC-Model/src/main/java/com/cydeo/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.cydeo.controller;
 
 import com.cydeo.Model.Employee;
+import com.github.javafaker.Faker;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -11,7 +15,24 @@ public class MainController {
     @RequestMapping("/employees")
     public String emplist(Model model){
 
-        model.addAttribute("name","MIKE");
+        //database' api' random data generator
+        List<Employee> employeeList = new ArrayList<>();
+        Faker faker = new Faker();
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+        employeeList.add(new Employee(faker.name().firstName(),faker.name().lastName(),faker.number().numberBetween(50000,140000)));
+
+        //model.addAttribute("name","MIKE");
+        model.addAttribute("employees",employeeList);
 
         return "employee-list.html";
 
